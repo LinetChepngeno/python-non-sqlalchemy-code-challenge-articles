289f613e3aa60e47c002e49601789d563e905cf5
Linet Chepngeno <linetmutai@gmail.com>
2024-03-11 22:07:06 +0300

    update article test file

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,28 @@
 class Article:
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Cannot modify the title attribute")
         
 class Author:
     def __init__(self, name):
         self.name = name
+        self.articles = []
+
+    def add_article(self, magazine, title):
+        article = Article(self, magazine, title)
+        self.articles.append(article)
+        return article
+
+    def topic_areas(self):
+        return list(set(article.magazine.category for article in self.articles))
 
     def articles(self):
         pass
@@ -24,6 +40,18 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.articles = []
+
+    def article_titles(self):
+        return [article.title for article in self.articles]
+
+    def contributing_authors(self):
+        author_counts = {}
+        for article in self.articles:
+            author = article.author
+            author_counts[author] = author_counts.get(author, 0) + 1
+
+        return [author for author, count in author_counts.items() if count > 2]
 
     def articles(self):
         pass
